0fc599c65537c451dffb1c7326107272f6c59975
SG-GIT-HUB <69902583+SG-GIT-HUB@users.noreply.github.com>
2021-10-28 08:21:14 +0200

    lesson 3

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,10 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+class Storepicker extends React.Component {
+    render() {
+        return <p>Hello!</p>
+    }
+}
+
+render(<p>HEYYYYY</p>, document.querySelector('#main'));
