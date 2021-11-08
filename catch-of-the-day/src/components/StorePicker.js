89f1a4778405d465ff7e8705e7511870f754a98a
SG-GIT-HUB <stevie.griffith@gmail.com>
2021-11-08 18:07:19 +0100

    lesson ref fix

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { getFunName } from "../helpers";
-
 class StorePicker extends React.Component {
     myInput = React.createRef();
-
+  
     goToStore = event => {
-        event.preventDefault();
-        const storeName = this.myInput.value.value;
-        this.props.history.push(`/store/${storeName}`);
+      // 1. Stop the form from submitting
+      event.preventDefault();
+      // 2. Get text from input
+      console.log(this.myInput.current); // what user typed in
+      const storeName = this.myInput.current.value;
+      // 3. Change page to /store/whatever-they-entered
+      this.props.history.push(`/store/${storeName}`);
     };
     render() {
-        return (
-            <form className="store-selector" onSubmit={this.goToStore}>
-                <h2>Please Enter A Store</h2>
-                <input
-                    type="text"
-                    ref={this.myInput}
-                    required 
-                    placeholder="Store Name"
-                    defaultValue={getFunName()}
-                />
-                <button type="submit">Visit Store →</button>
-            </form>
-        )
+      return (
+        <form className="store-selector" onSubmit={this.goToStore}>
+          <h2>Please Enter A Store</h2>
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
+          <button type="submit">Visit Store -{'>'}</button>
+        </form>
+      );
     }
-}
+  }
 
 export default StorePicker;
