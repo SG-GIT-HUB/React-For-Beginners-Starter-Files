2f90b5182ed8f222dd1f69927dcbe67feaa42008
SG-GIT-HUB <69902583+SG-GIT-HUB@users.noreply.github.com>
2021-12-18 09:01:52 +0100

    lesson 23

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,8 +1,14 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { formatPrice } from "../helpers";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Order extends React.Component {
+    static propTypes = {
+        fishes: PropTypes.object,
+        order: PropTypes.object,
+        removeFromOrder: PropTypes.func,
+    }
     renderOrder = key => {
         const fish = this.props.fishes[key];
         const count = this.props.order[key];
@@ -28,7 +34,10 @@ class Order extends React.Component {
                 <li key={key}>
                     <span>
                         <TransitionGroup component="span" className="count">
-                            <CSSTransition classNames="count" key={count} timeout={{ enter: 5000, exit: 5000 }}>
+                            <CSSTransition
+                                classNames="count"
+                                key={count}
+                                timeout={{ enter: 500, exit: 500 }}>
                                 <span>{count}</span>
                             </CSSTransition>
                         </TransitionGroup>
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,32 +1,37 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { getFunName } from "../helpers";
+
 class StorePicker extends React.Component {
-    myInput = React.createRef();
-  
-    goToStore = event => {
-      // 1. Stop the form from submitting
-      event.preventDefault();
-      // 2. Get text from input
-      console.log(this.myInput.current); // what user typed in
-      const storeName = this.myInput.current.value;
-      // 3. Change page to /store/whatever-they-entered
-      this.props.history.push(`/store/${storeName}`);
-    };
-    render() {
-      return (
-        <form className="store-selector" onSubmit={this.goToStore}>
-          <h2>Please Enter A Store</h2>
-          <input
-            type="text"
-            ref={this.myInput}
-            required
-            placeholder="Store Name"
-            defaultValue={getFunName()}
-          />
-          <button type="submit">Visit Store -{'>'}</button>
-        </form>
-      );
-    }
+  myInput = React.createRef();
+  static propTypes = {
+    history: PropTypes.object
   }
+
+  goToStore = event => {
+    // 1. Stop the form from submitting
+    event.preventDefault();
+    // 2. Get text from input
+    console.log(this.myInput.current); // what user typed in
+    const storeName = this.myInput.current.value;
+    // 3. Change page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
+  render() {
+    return (
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
+          ref={this.myInput}
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">Visit Store -{'>'}</button>
+      </form>
+    );
+  }
+}
 
 export default StorePicker;
